0309e6b1f1dbe8e80cc08290d11bff53ac9e1a1c
Aliansyah Firdaus <aliansyah.it@gmail.com>
2022-06-11 20:15:36 +0700

    Create a New Workout

--- script.js
+++ script.js
@@ -12,9 +12,49 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Run extends Workout {
+  type = 'running';
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  type = 'cycling';
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
 class App {
   #maps;
   #mapEvent;
+  #workouts = [];
 
   constructor() {
     this._getPosition();
@@ -55,15 +95,59 @@ class App {
   }
 
   _newWorkout(e) {
-    e.preventDefault();
-
     inputDistance.value = '';
     inputDuration.value = '';
     inputCadence.value = '';
     inputElevation.value = '';
-
     const { lat, lng } = this.#mapEvent.latlng;
-    L.marker([lat, lng])
+    let workout;
+
+    const validInput = (...input) => input.every(el => Number.isFinite(el));
+    const positifInput = (...input) => input.every(el => Number.isFinite(el));
+
+    e.preventDefault();
+
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+
+    console.log(type);
+
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+
+      if (
+        !validInput(distance, duration, cadence) ||
+        !positifInput(distance, duration, cadence)
+      ) {
+        return alert('Masukan angka bulat');
+      }
+
+      workout = new Run([lat, lng], distance, duration, cadence);
+    }
+
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+
+      if (
+        !validInput(distance, duration, elevation) ||
+        !positifInput(distance, duration)
+      ) {
+        return alert('Masukan angka bulat');
+      }
+
+      workout = new Cycling([lat, lng], distance, duration, elevation);
+    }
+
+    this.#workouts.push(workout);
+    this.renderWorkoutsMarker(workout);
+
+    inputCadence.blur();
+    inputElevation.blur();
+  }
+
+  renderWorkoutsMarker(workout) {
+    L.marker(workout.coords)
       .addTo(this.#maps)
       .bindPopup(
         L.popup({
@@ -71,13 +155,11 @@ class App {
           minWidth: 100,
           autoClose: false,
           closeOnClick: false,
-          className: 'running-popup',
+          className: `${workout.type}-popup`,
         })
       )
       .setPopupContent('Workout Kuy')
       .openPopup();
-    inputCadence.blur();
-    inputElevation.blur();
   }
 }
 
